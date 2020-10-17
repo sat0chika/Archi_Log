619c5e0c618e5fed2880df362a7797e10b7df798
sat0chika <sa10chika@gmail.com>
2020-10-17 22:18:31 +0900

    1018

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,14 +4,11 @@ class PeopleController < ApplicationController
     @topic = Topic.find_by(id: params[:id])
     person = Person.find(params[:id])
     @person = Person.name
-    @topics = person.topics
+    @topics = person.topics.order("created_at DESC").limit(10)
     @icon = person.icon
-
     @person = Person.find_by(id: params[:id])
-
     @name = person.name
     @introduction = person.introduction
-    # @topics = person.topics.order("created_at DESC").limit(10)
     # @topics = Topic.where(person_id: @parson)
     # @person = Person.find(current_person.id)
   end
