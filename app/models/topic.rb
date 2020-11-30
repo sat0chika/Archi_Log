7aa2157b30b3a8ff738ed6c8e669673875f9ccf3
sat0chika <sa10chika@gmail.com>
2020-11-30 09:50:42 +0900

    1130

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 34 }
+  validates :title, presence: true, length: { maximum: 30 }
   validates :description, presence: true
-  validates :heading, presence: true
+  validates :heading, presence: true, length: { maximum: 140 }
   validates :image, presence: true
   mount_uploader :image, ImageUploader
 
