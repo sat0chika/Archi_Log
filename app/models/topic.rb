689c75643739379f8fb7808ab75873f41b22b788
sat0chika <sa10chika@gmail.com>
2020-12-02 22:46:57 +0900

    1202

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -27,13 +27,5 @@ class Topic < ApplicationRecord
     old_tags = current_tags - sent_tags
     new_tags = sent_tags - current_tags
 
-    #   old_tags.each do |old|
-    #     self.topic_tags.delete TopicTag.find_by(topic_tag_name: old)
-    #   end
-
-    #   new_tags.each do |new|
-    #     new_topic_tag = TopicTag.find_or_create_by(topic_tag_name: new)
-    #     self.topic_tags << new_topic_tag
-    #   end
   end
 end
